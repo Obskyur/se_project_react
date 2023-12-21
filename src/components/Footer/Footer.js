1a5a027dbaa1ff5efc098f3615bd953383e06c08
Tristan Miller <parkourman.miller@gmail.com>
2023-12-20 18:09:06 -0800

    rm: unnecessary imports and declarations

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,7 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import "./Footer.css";
 
 class Footer extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <footer>
