3a2617cc03654d4a72cfe6d7fa5375b528db3ba8
Obskyur <parkourman.miller@gmail.com>
2024-07-22 16:22:11 -0700

    feat: Sidebar variable u/n & pic

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,15 @@
 import "./Sidebar.css";
-import userPic from "../../images/userpic.png";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 function Sidebar() {
+  const currentUser = useContext(CurrentUserContext);
+
   return (
     <section className="sidebar">
       <div className="sidebar__user">
-        <img src={userPic} className="sidebar__user-pic" alt="User" />
-        <div className="sidebar__user-name">Terrence Tegegne</div>
+        <img src={currentUser.avatarUrl} className="sidebar__user-pic" alt="User" />
+        <div className="sidebar__user-name">{currentUser.name}</div>
       </div>
     </section>
   );
